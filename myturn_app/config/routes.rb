2952733de50badbb31bb6d04e92b08c33c6dc0fc
Renata <rca14@cin.ufpe.br>
2016-11-16 10:46:04 -0300

    Update routes.rb

--- myturn_app/config/routes.rb
+++ myturn_app/config/routes.rb
@@ -1,17 +1,16 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   get 'sessions/new'
-
   get 'users/new'
-
-  root 'static_pages#home'
-  get  '/help',    to: 'static_pages#help'
-  get  '/about',   to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :microposts
-  resources :users
+  
+  root 'static_pages#home'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 end
