7e22fb684c311fd9718cbdd314cbbfc32c12beb3
RENATA ALBUQUERQUE <rca14@cin.ufpe.br>
2016-11-27 17:01:21 +0000

    improvement

--- myturn_app/app/models/fornecedor.rb
+++ myturn_app/app/models/fornecedor.rb
@@ -1,6 +1,6 @@
 class Fornecedor < ActiveRecord::Base
  attr_accessor :remember_token, :activation_token, :reset_token
- validates :CNPJ, presence: true, length: { maximum: 14 }
+ validates :CNPJ, presence: true, length: { is: 14 }, uniqueness: true
  validates_presence_of :razaosocial, message: ' Não pode ser deixado em branco'
  validates_presence_of :CNPJ, message: ' Não pode ser deixado em branco'
  validates :razaosocial, length: { minimum: 1, message: ' Deve ter pelo menos 1 caracteres'}
